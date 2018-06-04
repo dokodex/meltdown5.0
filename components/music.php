7c3acf9cb589efd8b1f18e3f59de6c4db90a73d0
pfadarraweb@gmail.com <pfadarraweb@gmail.com>
2018-06-04 12:52:42 +0200

    design/music

---
+++ components/music.php
@@ -0,0 +1,31 @@
+<section class="section music">
+  <a class="music__head__link" href="" target="_blank">
+    <div class="music__head__grid">
+      <div class="music__head__description vcenter">NEW ALBUM <br> "FROM THIS DAY TO THE GRAVE" <br> OUT NOW</div>
+      <div class="music__head__album">
+        <img class="music__head__img" src="img/music/ftdttg.png" alt=""/>
+        <div class="music__head__rrss">
+          <?php require('imports/svg/spotify.svg'); ?>
+          <?php require('imports/svg/youtube.svg'); ?>
+        </div>
+      </div>
+    </div>
+  </a>
+  <div class="music__albums">
+    <div class="music__album music__album--ftdttg">
+      <a href="" target="_blank" class="music__album__link vcenter">
+        "FROM THIS DAY TO THE GRAVE"<br>2018
+      </a>
+    </div>
+    <div class="music__album music__album--irtdh">
+      <a href="" target="_blank" class="music__album__link vcenter">
+        "I REFUSE TO DIE HERE"<br>2016
+      </a>
+    </div>
+    <div class="music__album music__album--oxymoron">
+      <a href="" target="_blank" class="music__album__link vcenter">
+        "OXYMORON"<br>2015
+      </a>
+    </div>
+  </div>
+</section>
