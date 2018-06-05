36b4fa230b6998652803845c636e1c8934a6c03d
pfadarraweb@gmail.com <pfadarraweb@gmail.com>
2018-06-05 12:51:59 +0200

    design/home

---
+++ components/home.php
@@ -0,0 +1,23 @@
+<section class="section home">
+  <div class="home__grid">
+    <div><img class="home__img" src="img/meltdown-logos/meltdown-white.png" alt=""/></div>
+    <nav class="home__menu">
+      <a class="home__menu__link" href="">HOME</a>
+      <a class="home__menu__link" href="">STORE</a>
+      <a class="home__menu__link" href="">VIDEOS</a>
+      <a class="home__menu__link" href="">MUSIC</a>
+      <a class="home__menu__link" href="">CONTACT</a>  
+    </nav>
+    <div class="home__languages">
+      <a class="home__languages__link" href="">ESP | </a>
+      <a class="home__languages__link" href="">ENG | </a>
+      <a class="home__languages__link" href="">EUS</a>
+    </div>
+    <div class="home__rrss">
+      <a class="home__rrss__link" href="" target="_blank"><?php require('imports/svg/facebook.svg') ?></a>
+      <a class="home__rrss__link" href="" target="_blank"><?php require('imports/svg/instagram.svg') ?></a>
+      <a class="home__rrss__link" href="" target="_blank"><?php require('imports/svg/spotify.svg') ?></a>
+      <a class="home__rrss__link" href="" target="_blank"><?php require('imports/svg/youtube.svg') ?></a>
+    </div>
+  </div>
+</section>
