94044003601abd8395a0d1f474d84bedcab9f91d
pfadarraweb@gmail.com <pfadarraweb@gmail.com>
2018-06-07 00:13:03 +0200

    design/footer

--- footer.php
+++ footer.php
@@ -1,12 +1,12 @@
    <div class="footer">
     <div class="footer__flex">
       <div class="footer__info flex__box">
-        2018 (cc) example, by <a class="footer__info__link" href="" target="_blank">Dokodex</a>
+        <div class="footer__year footer__text">2018 (cc)</div><div class="footer__name footer__text">M  E  L  T  D  O  W  N</div><div class="footer__author footer__text">by <a class="footer__info__link" href="" target="_blank">Dokodex</a></div>
       </div>
       <div class="footer__rrss flex__box">
         <a class="footer__rrss__link footer__rrss__link--facebook" href="" target="_blank"><?php require('imports/svg/facebook.svg') ?></a>
         <a class="footer__rrss__link footer__rrss__link--instagram" href="" target="_blank"><?php require('imports/svg/instagram.svg') ?></a>
-        <a class="footer__rrss__link footer__rrss__link--twitter" href="" target="_blank"><?php require('imports/svg/twitter.svg') ?></a>
+        <a class="footer__rrss__link footer__rrss__link--spotify" href="" target="_blank"><?php require('imports/svg/spotify.svg') ?></a>
       </div>
     </div>
   </div>
--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <?php require_once('header.php'); ?>
-<?php require_once('components/videos.php'); ?>
+<?php require_once('components/contact.php'); ?>
 <?php require_once('footer.php'); ?>
