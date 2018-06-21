80913c19bfcdef8a76c344df0f38a20d4d923ca9
pablo <pfadarraweb@gmail.com>
2018-06-21 17:44:33 +0200

    fix/footer2

--- footer.php
+++ footer.php
@@ -1,12 +1,13 @@
    <div class="footer">
     <div class="footer__flex">
       <div class="footer__info footer__flex__box">
-        <div class="footer__year footer__text">2018 (cc)</div><div class="footer__name footer__text">M  E  L  T  D  O  W  N</div><div class="footer__author footer__text">by <a class="footer__info__link" href="" target="_blank">Dokodex</a></div>
+        <div class="footer__text">2018 (cc) &nbsp;&nbsp; M  E  L  T  D  O  W  N,&nbsp;&nbsp; by <a class="footer__info__link" href="" target="_blank">Dokodex</a></div>
       </div>
       <div class="footer__sponsors footer__flex__box">
         <a class="footer__sponsors__link footer__sponsors__link--atlas"><img class="footer__sponsors__img" src="img/footer/atlas.png" alt=""/></a>
         <a class="footer__sponsors__link footer__sponsors__link--imminence"><img class="footer__sponsors__img" src="img/footer/imminence.png" alt=""/></a>
-        <a class="footer__sponsors__link footer__sponsors__link--hxc"><img class="footer__sponsors__img" src="" alt=""/></a>
+        <a class="footer__sponsors__link footer__sponsors__link--agr"><img class="footer__sponsors__img" src="img/footer/artgates.png" alt=""/></a>
+        <a class="footer__sponsors__link footer__sponsors__link--hxc"><img class="footer__sponsors__img footer__sponsors__img--hxc" src="img/footer/hxc.png" alt=""/></a>
       </div>
       <div class="footer__rrss footer__flex__box">
         <a class="footer__rrss__link footer__rrss__link--facebook" href="" target="_blank"><?php require('imports/svg/facebook.svg') ?></a>
