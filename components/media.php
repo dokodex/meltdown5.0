36e52316758810e6509d9c2d747465119ba45727
dokodex <pfadarraweb@gmail.com>
2018-09-14 22:10:41 +0200

    fix/media

--- components/media.php
+++ components/media.php
@@ -2,16 +2,17 @@
   <div class="media__download media__box">
     <h1>DOWNLOAD ZONE</h1>
     <div class="media__download__downloads">
-      <div class="media__download__item"><a class="media__download__item--link" href=""><?php require('imports/svg/download.svg')?>Promo photos</a></div>
-      <div class="media__download__item"><a class="media__download__item--link" href=""><?php require('imports/svg/download.svg')?>Rider</a></div>
-      <div class="media__download__item"><a class="media__download__item--link" href=""><?php require('imports/svg/download.svg')?>Dossier</a></div>
-      <div class="media__download__item"><a class="media__download__item--link" href=""><?php require('imports/svg/download.svg')?>Band logos</a></div>
+      <div class="media__download__item"><a class="media__download__item--link" href="download/Meltdown_promo_photos.zip"><?php require('imports/svg/download.svg')?>Promo photos</a></div>
+      <div class="media__download__item"><a class="media__download__item--link" href="download/Meltdown_technical_rider.pdf"><?php require('imports/svg/download.svg')?>Rider</a></div>
+      <div class="media__download__item"><a class="media__download__item--link" href="download/Meltdown_dossiers.zip"><?php require('imports/svg/download.svg')?>Dossier</a></div>
+      <div class="media__download__item"><a class="media__download__item--link" href="download/Meltdown_logos.zip"><?php require('imports/svg/download.svg')?>Band logos</a></div>
     </div>
   </div>
   <div class="media__photo media__box">
+    <img class="" src="img/media/photo.jpg" alt=""/>
   </div>
-  <div class="media__spoty media__box">
   </div>
-  <div class="media__bio media__box">
-  </div>
+  <div class="media__spotify media__box">
+    <iframe class="media__frame" src="https://embed.spotify.com/?uri=spotify:album:3f0xvtNAHbXhtCgVyEFEF8" width="" height="350" frameborder="0" allowtransparency="true"></iframe>
+  </div>  
 </section>
