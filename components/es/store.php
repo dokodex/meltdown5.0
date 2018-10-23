20097fcb4f31bc34091696028e3e880f44256561
dokodex <pfadarraweb@gmail.com>
2018-10-23 11:15:37 +0200

    fix/store5

--- components/es/store.php
+++ components/es/store.php
@@ -4,44 +4,59 @@
     <div class="store__product">
       <a href="https://meltdownband.bigcartel.com/product/fire-circle" target="_blank" class="store__product__link">
         <img class="store__img" src="img/merch/fire-circle.jpg">
-        <div class="store__description vcenter">FIRE CIRCLE 23€</div>
+        <div class="store__description vcenter">FIRE CIRCLE 25€</div>
       </a>
     </div>
     <div class="store__product">
       <a href="https://meltdownband.bigcartel.com/product/titans" target="_blank" class="store__product__link">
         <img class="store__img" src="img/merch/titans.jpg">
-        <div class="store__description vcenter">TITANS 8€</div>
-      </a>
-    </div>
-    <div class="store__product">
-      <a href="https://meltdownband.bigcartel.com/product/black-plume" target="_blank" class="store__product__link">
-        <img class="store__img" src="img/merch/black-plume.jpg">
-        <div class="store__description vcenter">BLACK PLUME 8€</div>
+        <div class="store__description vcenter">TITANS 10€</div>
       </a>
     </div>
     <div class="store__product">
       <a href="https://meltdownband.bigcartel.com/product/hands" target="_blank" class="store__product__link">
         <img class="store__img" src="img/merch/hands.jpg">
-        <div class="store__description vcenter">HANDS 8€</div>
+        <div class="store__description vcenter">HANDS 10€</div>
       </a>
     </div>
     <div class="store__product">
       <a href="https://meltdownband.bigcartel.com/product/rhomb" target="_blank" class="store__product__link">
         <img class="store__img" src="img/merch/rhomb.jpg">
-        <div class="store__description vcenter">RHOMB 13€</div>
+        <div class="store__description vcenter">RHOMB 15€</div>
       </a>
     </div>
     <div class="store__product">
       <a href="https://meltdownband.bigcartel.com/product/wire" target="_blank" class="store__product__link">
         <img class="store__img" src="img/merch/wire.jpg">
-        <div class="store__description vcenter">WIRE 8€</div>
+        <div class="store__description vcenter">WIRE 10€</div>
       </a>
     </div>
     <div class="store__product">
       <a href="https://meltdownband.bigcartel.com/product/windbreaker" target="_blank" class="store__product__link">
         <img class="store__img" src="img/merch/wind.jpg">
-        <div class="store__description vcenter">WIND 18€</div>
+        <div class="store__description vcenter">WIND 20€</div>
       </a>
     </div>
+    <div class="store__product">
+      <a href="https://meltdownband.bigcartel.com/product/hoodie" target="_blank" class="store__product__link">
+        <img class="store__img" src="img/merch/hoddie.jpg">
+        <div class="store__description vcenter">HOODIE 25€</div>
+      </a>
+    </div>
+    <div class="store__product">
+      <a href="https://meltdownband.bigcartel.com/product/snapback" target="_blank" class="store__product__link">
+        <img class="store__img" src="img/merch/snapback.jpg">
+        <div class="store__description vcenter">SNAPBACK 12€</div>
+      </a>
+    </div>
+    <div class="store__product">
+      <a href="https://meltdownband.bigcartel.com/product/cd-from-this-day-to-the-grave" target="_blank" class="store__product__link">
+        <img class="store__img" src="img/merch/cd.jpg">
+        <div class="store__description vcenter">ALBUM 10€</div>
+      </a>
+    </div>
+  </div>
+  <div class="store__note">
+    <strong>NOTA - </strong>Hacemos los envíos todos los <strong>jueves</strong>.
   </div>
 </section>
