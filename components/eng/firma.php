ef298b318ee3acafa0ca55609a5ad980718a51af
dokodex <pfadarraweb@gmail.com>
2018-12-27 12:36:55 +0100

    update/footer

---
+++ components/eng/firma.php
@@ -0,0 +1,10 @@
+<section class="section section__firma">
+  <div class="firma__grid">
+    <div class="firma__div__img">
+    </div>
+    <div class="firma__info">
+    </div>
+    <div class="firma__rrss">
+    </div>
+  </div>
+</section>
