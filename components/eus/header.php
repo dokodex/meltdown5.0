964c9ce5dcd46bf1d3a65642466769cdd7e4dab9
dokodex <pfadarraweb@gmail.com>
2019-01-17 16:18:05 +0100

    deign/spotify

--- components/eus/header.php
+++ components/eus/header.php
@@ -28,14 +28,14 @@
       t.src=v;s=b.getElementsByTagName(e)[0];
       s.parentNode.insertBefore(t,s)}(window, document,'script',
       'https://connect.facebook.net/en_US/fbevents.js');
-      fbq('init', 'your-pixel-id-goes-here');
+      fbq('init', '222068805412607');
       fbq('track', 'PageView');
     </script>
-    <noscript>
-      <img height="1" width="1" style="display:none" 
-           src="https://www.facebook.com/tr?id=your-pixel-id-goes-here&ev=PageView&noscript=1"/>
-    </noscript>
+    <noscript><img height="1" width="1" style="display:none"
+      src="https://www.facebook.com/tr?id=222068805412607&ev=PageView&noscript=1"
+    /></noscript>
     <!-- End Facebook Pixel Code -->
+
 
   </head>
 
