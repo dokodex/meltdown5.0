859fd1695fa7a021b5c16c9c19b42592807f23ef
dokodex <pfadarraweb@gmail.com>
2019-01-22 11:59:11 +0100

    fix/past-shows1

--- components/eng/past-shows.php
+++ components/eng/past-shows.php
@@ -1,13 +1,15 @@
 <section class="section past-shows">
-  <h1 class="past-shows__title title">PAST SHOWS</h1>
-  <table class="past-shows__table">
-    <tr class="past-shows--row past-shows__table--head">
-      <td>Date</td>
-      <td>City</td>
-      <td class="tour-dates__venue">Venue</td>
-      <td class="tour-dates__country">Country</td>
-      <td class="tour-dates__bands">Bands</td>
-    </tr>
-      <?php tablePastShows(); ?>
-  </table>
+  <div class="past-shows__content">
+    <h1 class="past-shows__title title">PAST SHOWS</h1>
+    <table class="past-shows__table">
+      <tr class="past-shows--row past-shows__table--head">
+        <td>Date</td>
+        <td>City</td>
+        <td class="tour-dates__venue">Venue</td>
+        <td class="tour-dates__country">Country</td>
+        <td class="tour-dates__bands">Bands</td>
+      </tr>
+        <?php tablePastShows(); ?>
+    </table>
+  </div>
 </section>
